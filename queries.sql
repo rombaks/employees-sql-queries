637e0e38d53ad15677a8979766e63ee2af88418c
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-24 20:34:39 +0400

    Add max & min departments salary queries

--- queries.sql
+++ queries.sql
@@ -20,3 +20,29 @@ FROM
 WHERE
     hire_date >= CURRENT_DATE - interval '30 day';
 
+/*
+Найти максимальную и минимальную зарплату по каждому департаменту
+Вариант c ID департамента
+ */
+SELECT
+    department_id,
+    MIN(salary) min_salary,
+    MAX(salary) max_salary
+FROM
+    Employees
+GROUP BY
+    department_id;
+
+/*
+Найти максимальную и минимальную зарплату по каждому департаменту
+Вариант c именем департамента
+ */
+SELECT
+    d."name" AS department,
+    MIN(salary) min_salary,
+    MAX(salary) max_salary
+FROM
+    Employees e
+    RIGHT JOIN Departments d ON e.department_id = d.department_id
+GROUP BY
+    d."name";
